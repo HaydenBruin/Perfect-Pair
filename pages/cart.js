e7a538c53e4f8fc86731f1a44f2a0796e4fffc4c
Hayden Bruin <bruinhayden@gmail.com>
2018-10-02 22:16:27 +1300

    adding cart styles

--- pages/cart.js
+++ pages/cart.js
@@ -8,19 +8,44 @@ export default class Cart extends Component {
                 <div className="cart">
                     <h1>Your cart</h1>
 
-                    <div className="cart-list">
-                        <div className="column title">Item</div>
-                        <div className="column title"></div>
-                        <div className="column title">Price</div>
-                        <div className="column title">Quantity</div>
-                        <div className="column title">Total</div>
+                    <table class="cartlist">
+                        <thead>
+                            <tr class="border">
+                                <th class="desc">Item description</th>
+                                <th>Price</th>
+                                <th>Quantity</th>
+                                <th><strong>Total</strong></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <form id="cart-form" action="?" method="POST"></form>
+                            <input type="hidden" name="submitted" value="1" />
 
-                        <div className="column"><img src="/static/socks/product1.jpg" className="productimg" /></div>
-                        <div className="column">Captain America socks</div>
-                        <div className="column">$11.99<br />$6.99</div>
-                        <div className="column"><input type="text" defaultValue="1" /></div>
-                        <div className="column">$13.98</div>
-                    </div>
+                                <tr class="border">
+                                    <td class="image">
+                                        <img src="/static/socks/product1.jpg" alt="Mens Berghaus Explorer Active GTX Shoe" />
+
+                                        <p><strong>Mens Berghaus Explorer Active GTX Shoe</strong></p>
+                                        <p>Colour: Black/Red</p><p> Shoe Size: UK 9</p>
+						
+					                </td>
+                                    <td><strong>$105.00</strong></td>
+                                    <td>
+                                        <label class="hide" for="qty1">Quantity</label>
+                                        <input type="hidden" name="products[2945]" id="inv" value="2945" />
+                                        <input type="text" name="products[2945]" id="qty" value="1" />
+                                        <a href="#" class="cart-update">Update</a>
+                                        <a href="#" class="cart-remove" pid="2945">Remove</a>
+					                </td>
+                                    <td>
+                                        <strong class="oldprice">$115.00</strong>
+                                        <strong class="onsale">$105.00</strong>
+
+                                        <p class="discount">You save<br />$10.00 (8.70%)</p>
+                                    </td>
+					            </tr>
+						</tbody>
+					</table>
                 </div>
             </DefaultLayout>
         )
