a00820fdf69f4444e819a63e62c242b3923b1944
Hayden Bruin <bruinhayden@gmail.com>
2018-10-02 23:53:34 +1300

    Setting up cart design

--- pages/cart.js
+++ pages/cart.js
@@ -1,53 +1,52 @@
 import React, { Component } from 'react'
-import DefaultLayout from '../components/layouts/default-layout'
 
 export default class Cart extends Component {
     render() {
         return (
-            <DefaultLayout>
+            <div className="checkout">
                 <div className="cart">
-                    <h1>Your cart</h1>
-
-                    <table class="cartlist">
+                    <img src="/static/logo-circle-pp-blue.png" alt="Perfect Pair Logo" className="logo" />
+                    <table className="cartlist">
                         <thead>
-                            <tr class="border">
-                                <th class="desc">Item description</th>
+                            <tr className="border">
+                                <th className="desc">Item description</th>
                                 <th>Price</th>
                                 <th>Quantity</th>
                                 <th><strong>Total</strong></th>
                             </tr>
                         </thead>
                         <tbody>
-                            <form id="cart-form" action="?" method="POST"></form>
-                            <input type="hidden" name="submitted" value="1" />
 
-                                <tr class="border">
-                                    <td class="image">
-                                        <img src="/static/socks/product1.jpg" alt="Mens Berghaus Explorer Active GTX Shoe" />
+                            <tr className="border">
+                                <td className="image">
+                                    <img src="/static/socks/product1.jpg" alt="Product Title" />
 
-                                        <p><strong>Mens Berghaus Explorer Active GTX Shoe</strong></p>
-                                        <p>Colour: Black/Red</p><p> Shoe Size: UK 9</p>
-						
-					                </td>
-                                    <td><strong>$105.00</strong></td>
-                                    <td>
-                                        <label class="hide" for="qty1">Quantity</label>
-                                        <input type="hidden" name="products[2945]" id="inv" value="2945" />
-                                        <input type="text" name="products[2945]" id="qty" value="1" />
-                                        <a href="#" class="cart-update">Update</a>
-                                        <a href="#" class="cart-remove" pid="2945">Remove</a>
-					                </td>
-                                    <td>
-                                        <strong class="oldprice">$115.00</strong>
-                                        <strong class="onsale">$105.00</strong>
+                                    <p><strong>Captain America</strong></p>
+                    
+                                </td>
+                                <td><strong>$105.00</strong></td>
+                                <td>
+                                    <label className="hide" for="qty1">Quantity</label>
+                                    <input type="hidden" name="products[2945]" id="inv" value="2945" />
+                                    <input type="text" name="products[2945]" id="qty" value="1" />
+                                    <a href="#" className="cart-update">Update</a>
+                                    <a href="#" className="cart-remove" pid="2945">Remove</a>
+                                </td>
+                                <td>
+                                    <strong className="oldprice">$115.00</strong>
+                                    <strong className="onsale">$105.00</strong>
 
-                                        <p class="discount">You save<br />$10.00 (8.70%)</p>
-                                    </td>
-					            </tr>
-						</tbody>
-					</table>
+                                    <p className="discount">You save<br />$10.00 (8.70%)</p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
-            </DefaultLayout>
+                
+                <div className="payment">
+                    payment..
+                </div>
+            </div>
         )
     }
 }
