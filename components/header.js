ab16ff1160c5c25250106b49c8cbb818b9f8c786
Hayden Bruin <bruinhayden@gmail.com>
2018-10-08 00:10:21 +1300

    Updating header, adding account bar

--- components/header.js
+++ components/header.js
@@ -1,23 +1,28 @@
-import React, { Component } from 'react'
+import React, { Fragment, Component } from 'react'
 import Link from 'next/link'
 
 export default class Header extends Component {
 
     render() {
         return (
-            <header>
-                <div className="logo column">
-                    <Link href="/"><a><img src="/static/logo-circle-pp-blue.png" alt="Perfect Pair Logo" /></a></Link>
+            <Fragment>
+                <div className="accountbar">
+
                 </div>
-                <div className="nav column">
-                    <nav>
-                        <ul>
-                            <li><a>Account</a></li>
-                            <li><Link href="/checkout"><a>Cart: 0 items</a></Link></li>
-                        </ul>
-                    </nav>
-                </div>
-            </header>
+                <header>
+                    <div className="logo column">
+                        <Link href="/"><a><img src="/static/logo-white-nocircle.png" alt="Perfect Pair Logo" /></a></Link>
+                    </div>
+                    <div className="nav column">
+                        <nav>
+                            <ul>
+                                <li><a>Account</a></li>
+                                <li><Link href="/checkout"><a>Cart: 0 items</a></Link></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </header>
+            </Fragment>
         )
     }
 }
