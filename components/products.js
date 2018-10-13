9d9d9345ddbd80fd366d34990334c3852a805121
Hayden Bruin <bruinhayden@gmail.com>
2018-10-13 14:10:18 +1300

    Setting up .env file

--- components/products.js
+++ components/products.js
@@ -9,10 +9,9 @@ export default class Products extends Component {
     }
 
     componentDidMount = () => {
-        console.log('fetching')
-        if(!this.state.products)
+        if(this.state.products.length === 0)
         {
-            fetch('http://localhost:8000/api/products/', {
+            fetch(`${process.env.API_URL}/api/products/`, {
                 method: 'get',
                 headers: new Headers({
                     'Content-Type': 'application/json'
@@ -20,94 +19,31 @@ export default class Products extends Component {
             }).then(response => response.json())
             .then(data => this.setState({ products: data }));
         }
-        console.log(this.state);
     }
 
     render() {
-        console.log('render');
-        console.log(this.state);
         return (
             <div className="products">
                 <div className="products-list">
                     {
-                        /*this.products.map((product, index) => {
+                        this.state.products.map((product, index) => {
                             return (
                                 <div className="product" key={index}>
-                                    <Link href={`/product/${product.slug}`}><img src={product.img} className="productimg"/></Link>
+                                    <Link href={`/product/${product.slug}`}><img src={product.image} className="productimg"/></Link>
                                     <div className="details">
                                         <h2>{product.title}</h2>
                                         <div className="pricing">
-                                            <div className="saleprice">${product.salePrice}</div>
+                                            <div className="saleprice">${product.saleprice}</div>
                                             <div className="price">${product.price}</div>
                                         </div>
                                         <AddToCart product={product} />
                                     </div>
                                 </div>
                             )
-                        })*/
+                        })
                     }
                 </div>
             </div>
         )
     }
 }
-
-/*
-products = [
-        {
-            id: 1,
-            title: "Captain America",
-            slug: 'captain-america',
-            img: "/static/socks/product1.jpg",
-            price: 11.99,
-            salePrice: 6.99,
-            inventory: 2
-        },
-        {
-            id: 2,
-            title: "Pikachu Socks",
-            slug: 'pikachu-socks',
-            img: "/static/socks/product2.jpg",
-            price: 11.99,
-            salePrice: 6.99,
-            inventory: 2
-        },
-        {
-            id: 3,
-            title: "Avocado Lovers",
-            slug: 'avocado-loves',
-            img: "/static/socks/product3.jpg",
-            price: 11.99,
-            salePrice: 6.99,
-            inventory: 2
-        },
-        {
-            id: 4,
-            title: "Spiderman",
-            slug: 'spiderman',
-            img: "/static/socks/product4.jpg",
-            price: 11.99,
-            salePrice: 6.99,
-            inventory: 2
-        },
-        {
-            id: 5,
-            title: "Captain America",
-            slug: 'captain-america',
-            img: "/static/socks/product1.jpg",
-            price: 11.99,
-            salePrice: 6.99,
-            inventory: 2
-        },
-        {
-            id: 6,
-            title: "Pikachu Socks",
-            slug: 'pikachu-socks',
-            img: "/static/socks/product2.jpg",
-            price: 11.99,
-            salePrice: 6.99,
-            inventory: 2
-            
-        }
-    ]
-*/
--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+const { parsed: localEnv } = require('dotenv').config()
+const webpack = require('webpack')
 const withSass = require('@zeit/next-sass')
 
-module.exports = withSass({useFileSystemPublicRoutes: false})
+module.exports = withSass({
+    useFileSystemPublicRoutes: false,
+    webpack: (config) => {
+        config.plugins.push(
+          new webpack.EnvironmentPlugin(localEnv)
+        )
+        return config
+    }
+});
