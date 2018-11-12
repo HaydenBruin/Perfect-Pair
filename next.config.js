3edb284a07e95dd7129ac28870c2f4dd839b84fa
Hayden Bruin <Hayden.Bruin@cucumber.co.nz>
2018-11-12 13:46:41 +1300

    Updating build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 const { parsed: localEnv } = require('dotenv').config()
-const routes = require('./routes')
 const webpack = require('webpack')
 const withSass = require('@zeit/next-sass')
 
 module.exports = withSass({
     useFileSystemPublicRoutes: false,
-    exportPathMap: routes,
     webpack: (config) => {
         config.plugins.push(
           new webpack.EnvironmentPlugin(localEnv)
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,12 @@ export default class index extends Component {
     render() {
         return (
             <DefaultLayout>
+            <p>NODE_ENV value is "{process.env.APU_URL}"</p>
+                <p>NODE_ENV value is "{process.env.NODE_ENV}"</p>
+        <p>CUSTOM_ENV_VAR value is "{process.env.CUSTOM_ENV_VAR}"</p>
+        <p>REACT_APP_CUSTOM_ENV_VAR value is "{process.env.REACT_APP_CUSTOM_ENV_VAR}"</p>
+        <p>TOML_ENV_VAR value is "{process.env.TOML_ENV_VAR}"</p>
+        <p>REACT_APP_TOML_ENV_VAR value is "{process.env.REACT_APP_TOML_ENV_VAR}"</p>
                 <Products />
             </DefaultLayout>
         )
