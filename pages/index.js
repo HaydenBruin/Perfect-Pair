efbc9c8ae6b1c91eb579d318d55fcf6276caa9c0
Hayden Bruin <Hayden.Bruin@cucumber.co.nz>
2018-11-12 14:11:55 +1300

    Checking process env vars

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,15 @@ import DefaultLayout from '../components/layouts/default-layout'
 import Products from '../components/products'
 
 export default class index extends Component {
+    constructor() {
+        super();
+        console.log('REACT_APP_API_URL: ' + process.env.REACT_APP_API_URL);
+        console.log('CUSTOM_API_URL: ' + process.env.CUSTOM_API_URL);
+        console.log('API_URL: ' + process.env.API_URL);
+    }
     render() {
         return (
             <DefaultLayout>
-                <p>API_KEY value is "{process.env.REACT_APP_API_URL}"</p>
-                <p>API_KEY value is "{process.env.API_URL}"</p>
                 <Products />
             </DefaultLayout>
         )
