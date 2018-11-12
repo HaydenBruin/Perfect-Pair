11023e079d9990c776c207c5bf9898d259086ed5
Hayden Bruin <Hayden.Bruin@cucumber.co.nz>
2018-11-12 13:20:20 +1300

    Updating netlify.toml

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const { parsed: localEnv } = require('dotenv').config({ systemvars: true })
-const getRoutes = require('./routes')
+const { parsed: localEnv } = require('dotenv').config()
+const routes = require('./routes')
 const webpack = require('webpack')
 const withSass = require('@zeit/next-sass')
 
 module.exports = withSass({
     useFileSystemPublicRoutes: false,
-    exportPathMap: getRoutes,
+    exportPathMap: routes,
     webpack: (config) => {
         config.plugins.push(
           new webpack.EnvironmentPlugin(localEnv)
