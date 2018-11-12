a41e0f2df81c21f31c8cf12944a82a7db2322e6a
Hayden Bruin <Hayden.Bruin@cucumber.co.nz>
2018-11-12 13:51:39 +1300

    Fixing var spelling mistake

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default class index extends Component {
     render() {
         return (
             <DefaultLayout>
-            <p>NODE_ENV value is "{process.env.APU_URL}"</p>
+            <p>API_KEY value is "{process.env.API_KEY}"</p>
                 <p>NODE_ENV value is "{process.env.NODE_ENV}"</p>
         <p>CUSTOM_ENV_VAR value is "{process.env.CUSTOM_ENV_VAR}"</p>
         <p>REACT_APP_CUSTOM_ENV_VAR value is "{process.env.REACT_APP_CUSTOM_ENV_VAR}"</p>
