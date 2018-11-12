6983bf9ee5e3a35c03653f857f5e80bbd191ba4c
Hayden Bruin <Hayden.Bruin@cucumber.co.nz>
2018-11-12 14:58:47 +1300

    testing react_app_custom

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default class index extends Component {
     constructor() {
         super();
         console.log('REACT_APP_API_URL: ' + process.env.REACT_APP_API_URL);
+        console.log('REACT_APP_CUSTOM_API_URL: ' + process.env.REACT_APP_CUSTOM_API_URL);
         console.log('CUSTOM_API_URL: ' + process.env.CUSTOM_API_URL);
         console.log('API_URL: ' + process.env.API_URL);
     }
