ab67064efd8a53487da3595df20e1a6eb1a2231a
Hayden Bruin <bruinhayden@gmail.com>
2018-12-09 23:47:18 +1300

    readding .env (build requires it)

--- next.config.js
+++ next.config.js
@@ -6,6 +6,12 @@ const prod = process.env.NODE_ENV === 'production';
 
 module.exports = withSass({
     useFileSystemPublicRoutes: false,
+    webpack: (config) => {
+        config.plugins.push(
+          new webpack.EnvironmentPlugin(localEnv)
+        )
+        return config
+    },
     publicRuntimeConfig: {
         API_URL: 'https://api.perfectpair.nz',
         STRIPE_PUBLIC: prod ? 'pk_live_h3O9uDR1sgFrz1sLfbU6QynV' : 'pk_test_xxaqpzviIbXJ63m1TPUhoyz8'
