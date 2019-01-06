b573636a24609b859949741ed8498417eddfeb18
Hayden Bruin <bruinhayden@gmail.com>
2019-01-06 13:26:47 +1300

    Removing sale from banner

--- components/header.js
+++ components/header.js
@@ -12,7 +12,7 @@ class Header extends Component {
         discountFinish: 1546243541807,
         currentMessage: 0,
         messages: [
-            "All novetly socks just $5.99 (usually $9.99), only until Christmas!"
+            "All novetly socks just $5.99 (usually $9.99)"
             //"Buy 2 pairs of socks get 15% off, buy 3 pairs get 30% off"
         ]
     }
@@ -63,7 +63,7 @@ class Header extends Component {
                 <div className="secondary-header">
                     <div className="container">
                         <div className="message"><span className="sale">Opening Sale</span> {this.state.messages[0]}</div>
-                        <div className="countdown"><Countdown date={this.state.discountFinish} /> till sale ends</div>
+                        <div className="countdown" style={{display: 'none'}}><Countdown date={this.state.discountFinish} /> till sale ends</div>
                     </div>
                 </div>
                 {!this.props.checkoutPage ? (
--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,6 @@ export default class index extends Component {
     render() {
         return (
             <DefaultLayout>
-                <div className="christmas-delivery">
-                    <p>It's too late for christmas but you could be looking snazzy in the new year!</p>
-                </div>
                 <Products products={this.props.products} />
             </DefaultLayout>
         )
